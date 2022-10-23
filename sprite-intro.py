e1fc8a6b7cddac9985e8771064f82b44700b5da6
Dan <confused.jpg@pop-os.localdomain>
2022-10-23 23:06:24 +0200

    finished crosshair
    added crosshair sprite, linked it to mouse position and added a shooting sound & cleaned code

--- sprite-intro.py
+++ sprite-intro.py
@@ -1,31 +1,48 @@
 #credits to Clear Code for the tutorial
-import pygame, sys
+import pygame, sys, os
 
+#crosshair class
 class Crosshair(pygame.sprite.Sprite): #inherit from pygame.sprite.Sprite
-    def __init__(self, width, height, x, y, color):
+    def __init__(self, path):
         super().__init__()
         
-        self.image = pygame.Surface((width, height))
-        self.image.fill(color)
-        self.rect = self.image.get_rect(center=(x,y))
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect()
+        self.pew = pygame.mixer.Sound(os.path.join("art", "pew.wav"))
+        self.pew.set_volume(0.2)
+
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
+
+    def shoot(self):
+        self.pew.play()
 
 pygame.init()
 clock = pygame.time.Clock()
 
-WIDTH, HEIGHT = 900,800
+WIDTH, HEIGHT = 800,800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sprite Introduction")
 
-crosshair = Crosshair(40,40,WIDTH//2, HEIGHT//2, (155,255,255))
-chair_group = pygame.sprite.Group() #a sprite group, the only way to display sprites
-chair_group.add(crosshair)
+#crosshair stuff
+crosshair = Crosshair(os.path.join("art", "crosshair.png"))
+crosshair_group = pygame.sprite.Group() #a sprite group, the only way to display sprites
+crosshair_group.add(crosshair)
 
+#images
+background = pygame.image.load(os.path.join("art", "background.png"))
+
+pygame.mouse.set_visible(False) #hide mouse
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            crosshair.shoot()
         
-    chair_group.draw(screen) #draw sprites of chair_group
+    screen.blit(background, (0,0)) #display background
+    crosshair_group.draw(screen) #draw sprites of chair_group
+    crosshair_group.update()
     pygame.display.flip()
     clock.tick(60)
