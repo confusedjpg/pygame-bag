e470d583c21e3bfe663131fe3af2a435ad11a25b
Dan <confused.jpg@pop-os.localdomain>
2022-10-23 21:27:04 +0200

    created and drew a basic sprite

--- sprite-intro.py
+++ sprite-intro.py
@@ -1,12 +1,24 @@
 #credits to Clear Code for the tutorial
 import pygame, sys
 
+class Crosshair(pygame.sprite.Sprite): #inherit from pygame.sprite.Sprite
+    def __init__(self, width, height, x, y, color):
+        super().__init__()
+        
+        self.image = pygame.Surface((width, height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(center=(x,y))
+
 pygame.init()
 clock = pygame.time.Clock()
 
-WIDTH, HEIGHT = 900,900
+WIDTH, HEIGHT = 900,800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sprite Introduction")
+
+crosshair = Crosshair(40,40,WIDTH//2, HEIGHT//2, (155,255,255))
+chair_group = pygame.sprite.Group() #a sprite group, the only way to display sprites
+chair_group.add(crosshair)
 
 while 1:
     for event in pygame.event.get():
@@ -14,5 +26,6 @@ while 1:
             pygame.quit()
             sys.exit()
         
+    chair_group.draw(screen) #draw sprites of chair_group
     pygame.display.flip()
     clock.tick(60)
