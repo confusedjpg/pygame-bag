3401996076f289d7fda169af3e3be4520dbf4fe9
confusedjpg <dansajin06@gmail.com>
2022-10-24 13:06:10 +0200

    adjusted size and speed animation & linked it to a button press

--- sprite-animation-intro.py
+++ sprite-animation-intro.py
@@ -1,8 +1,8 @@
 #credits to Clear Code for the tutorial
-import pygame, sys
+import pygame, sys, os
 
 #constants and window
-WIDTH, HEIGHT = 800,800
+WIDTH, HEIGHT = 300,300
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sprite Animation Introduction")
@@ -12,9 +12,25 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((20,20))
-        self.image.fill((155,255,255))
-        self.rect = self.image.get_rect(center = (x,y))
+        self.sprites = [] #container for the sprites
+        for nr in range(1,11):
+            self.sprites.append(pygame.image.load(os.path.join("art", f"attack_{nr}.png"))) #load all frames
+        self.current_sprite = 0
+        self.image = pygame.transform.scale2x(self.sprites[self.current_sprite]) #set sprite to initial frame
+
+        self.rect = self.image.get_rect(center=(x,y))
+        self.is_animating = False
+
+    def update(self, speed): #run animation
+        if self.is_animating == True:
+            self.current_sprite += speed #.1 instead of 1 to slow down the animation to a decent speed
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+            self.image = pygame.transform.scale2x(self.sprites[int(self.current_sprite)])
+
+    def animate(self):
+        self.is_animating = True
 
 #create player and add it to sprite group
 player = Player(WIDTH//2, HEIGHT//2)
@@ -27,7 +43,12 @@ while 1:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYDOWN: #run animation only on keypress
+            player.animate()
     
     pygame.display.flip()
+    screen.fill((0,0,0))
+    player_group.update(.25) #change frames
     player_group.draw(screen) #draw player
     clock.tick(60)
