0b00fa3b9489c87d2596568cb38b60be5849a9dd
confusedjpg <dansajin06@gmail.com>
2022-10-24 21:07:07 +0200

    finished demo
    refined some code and added focused/unfocused state so you can stop typing

--- text-input.py
+++ text-input.py
@@ -7,6 +7,11 @@ pygame.display.set_caption("Text Input Demo")
 
 user_text = ""
 
+input_rect = pygame.Rect(100,100,140,32) #create rect
+color_active = (150,255,255)
+color_passive = (50,50,50)
+active = False
+
 clock = pygame.time.Clock()
 while 1:
     for event in pygame.event.get():
@@ -14,14 +19,21 @@ while 1:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            active = True if input_rect.collidepoint(event.pos) else False #focused or not
+
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_BACKSPACE: #add possibility to remove text
-                user_text = user_text[:-1]
-            else:
-                user_text += event.unicode #render the pressed keys
+            if active: #only if the textbox is focused
+                if event.key == pygame.K_BACKSPACE: #add possibility to remove text
+                    user_text = user_text[:-1]
+                else:
+                    user_text += event.unicode #render the pressed keys
     
     screen.fill((0,0,0))
+    pygame.draw.rect(screen, color_active if active else color_passive, input_rect, 2) #draw the rect, the 2 is for an outline
     text_surf = pygame.font.Font(None, 32).render(user_text, True, (255,255,255))
-    screen.blit(text_surf, (0,0))
+
+    input_rect.w = max(100, text_surf.get_width()+10) #change rect width accordingly, 100 is the minimum width
+    screen.blit(text_surf, (input_rect.x+5, input_rect.y+5))
     pygame.display.flip()
     clock.tick(60)
