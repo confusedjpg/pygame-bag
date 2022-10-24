5743e5d2162dc5844349514ef438049d79ae3335
confusedjpg <dansajin06@gmail.com>
2022-10-24 18:33:25 +0200

    added basic text input and rendering

--- text-input.py
+++ text-input.py
@@ -1,9 +1,11 @@
 import pygame, sys
 
-WIDTH, HEIGHT = 600,600
+WIDTH, HEIGHT = 400,400
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Text Input Demo")
+
+user_text = ""
 
 clock = pygame.time.Clock()
 while 1:
@@ -11,7 +13,15 @@ while 1:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE: #add possibility to remove text
+                user_text = user_text[:-1]
+            else:
+                user_text += event.unicode #render the pressed keys
     
     screen.fill((0,0,0))
+    text_surf = pygame.font.Font(None, 32).render(user_text, True, (255,255,255))
+    screen.blit(text_surf, (0,0))
     pygame.display.flip()
     clock.tick(60)
